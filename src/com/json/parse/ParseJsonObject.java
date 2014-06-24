d6fcd8bc8e6e9cffd8eea63e91c162bf46160325
astomusic <astomusic@naver.com>
2014-06-24 23:31:49 +0900

    #6

---
+++ src/com/json/parse/ParseJsonObject.java
@@ -0,0 +1,29 @@
+package com.json.parse;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+/**
+ * 
+ * @brief json parser
+ * @detail 설정한 depth에 따라 넘어온 자료를 파싱한다.
+ *
+ */
+public class ParseJsonObject {
+	public JSONObject trimJobj (JSONObject inputjson, String[] depth) {
+		String jsonstr = "";
+		JSONObject jsonobj = inputjson;
+		
+		try {	
+			for(int i = 0 ; i < depth.length ; i++)
+			{
+				jsonstr = jsonobj.getString(depth[i]);
+				jsonobj = new JSONObject(jsonstr);
+			}
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return jsonobj;
+	}
+}
